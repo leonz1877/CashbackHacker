3cfb5e247df690c85fa9846ba1e9c62c37ed852d
Leonid Zhdanov <zhdanov.leon@gmail.com>
2022-08-02 21:47:16 +0400

    add test class junit4-platform

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,9 +1,8 @@
 package ru.netology.service;
 
-import org.junit.Assert;
-import org.junit.Test;
-
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.*;
 
 public class CashbackHackServiceTest {
 
@@ -11,7 +10,7 @@ public class CashbackHackServiceTest {
     int amount;
 
     @Test
-    public void amount900 () {
+    public void amount900JUnit4 () {
         amount = 900;
 
         int expected = 100;
@@ -22,7 +21,7 @@ public class CashbackHackServiceTest {
     }
 
     @Test
-    public void amount999 () {
+    public void amount999JUnit4 () {
         amount = 999;
 
         int expected = 1;
@@ -33,7 +32,7 @@ public class CashbackHackServiceTest {
     }
 
     @Test
-    public void amount1000 () {
+    public void amount1000JUnit4 () {
         amount = 1000;
 
         int expected = 0;
@@ -44,7 +43,7 @@ public class CashbackHackServiceTest {
     }
 
     @Test
-    public void amount1001 () {
+    public void amount1001JUnit4 () {
         amount = 1001;
 
         int expected = 999;
@@ -55,13 +54,68 @@ public class CashbackHackServiceTest {
     }
 
     @Test
-    public void amount1100 () {
+    public void amount1100JUnit4 () {
         amount = 1100;
 
         int expected = 900;
         int actual = cashbackHackService.remain(amount);
 
         Assert.assertEquals (expected,actual);
+
+    }
+
+    @Test
+    public void amount900JUnitJupiter () {
+        amount = 900;
+
+        int expected = 100;
+        int actual = cashbackHackService.remain(amount);
+
+        Assertions.assertEquals (expected,actual);
+
+    }
+
+    @Test
+    public void amount999JUnitJupiter () {
+        amount = 999;
+
+        int expected = 1;
+        int actual = cashbackHackService.remain(amount);
+
+        Assertions.assertEquals (expected,actual);
+
+    }
+
+    @Test
+    public void amount1000JUnitJupiter () {
+        amount = 1000;
+
+        int expected = 0;
+        int actual = cashbackHackService.remain(amount);
+
+        Assertions.assertEquals (expected,actual);
+
+    }
+
+    @Test
+    public void amount1001JUnitJupiter () {
+        amount = 1001;
+
+        int expected = 999;
+        int actual = cashbackHackService.remain(amount);
+
+        Assertions.assertEquals (expected,actual);
+
+    }
+
+    @Test
+    public void amount1100JUnitJupiter () {
+        amount = 1100;
+
+        int expected = 900;
+        int actual = cashbackHackService.remain(amount);
+
+        Assertions.assertEquals (expected,actual);
 
     }
 
